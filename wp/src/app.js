0f73cc4e81d0a17cea0f97cd3fd4beb01777b0c6
raika <>
2019-06-28 02:33:37 +0900

    DOM取得の修正

--- wp/src/app.js
+++ wp/src/app.js
@@ -7,10 +7,12 @@ import Menu from "./js/menu";
 
 document.addEventListener('DOMContentLoaded', function(){
 	var formDom = document.getElementsByClassName('contact-form');
+	if(formDom.length !== 0){
 	for (var i = 0; i < formDom.length; i++){
     ReactDOM.render(
         <Form />, formDom[i]
     );
+	}
 	}
 	
 	var menuDom = document.getElementById('js-menu');
